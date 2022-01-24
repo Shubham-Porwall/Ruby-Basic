84b7fa6e4039f7012973f3752aaa7ab7b74841f1
Shubham Porwall <sporwal@grepruby.io>
2022-01-24 16:36:06 +0530

    modified

--- file_manage.rb
+++ file_manage.rb
@@ -1,15 +1,15 @@
-afile = File.open("test.rb" , "r+")
+afile = File.open('test.rb' , 'r+')
 if afile
   afile.syswrite('hello GAMMASTACK')
 else
-  puts "unavailable"
+  puts 'unavailable'
 end
 afile.close
 
-bfile = File.open("test.rb" , "r")
+bfile = File.open('test.rb', 'r')
 if bfile
   con = bfile.sysread(50)
   puts con
 else
-  puts "Unavailable"
+  puts 'Unavailable'
 end
